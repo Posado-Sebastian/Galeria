95c9184dff95e3b48cfc185631571bba556271b5
Posado-Sebastian <sebastianposado@gmail.com>
2025-08-04 18:40:00 +0000

    navbar

---
+++ src/app/navbar/navbar.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import {
+  CollapseDirective,
+  ContainerComponent,
+  NavbarBrandDirective,
+  NavbarComponent,
+  NavbarNavComponent,
+  NavbarTogglerDirective,
+  NavItemComponent,
+  NavLinkDirective
+} from '@coreui/angular';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.html',
+  standalone: true,
+  imports: [
+    NavbarComponent,
+    ContainerComponent,
+    NavbarBrandDirective,
+    NavbarTogglerDirective,
+    CollapseDirective,
+    NavbarNavComponent,
+    NavItemComponent,
+    NavLinkDirective
+  ]
+})
+export class Navbar {
+
+}
