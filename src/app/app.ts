bd359ca84d6f4c447c9ec309b7eb6d1c7aed9892
Posado-Sebastian <sebastianposado@gmail.com>
2025-08-04 19:54:31 +0000

    about me section

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,11 +4,12 @@ import { RouterOutlet } from '@angular/router';
 import { Navbar } from './navbar/navbar';
 import { Carousel } from './carousel/carousel';
 import { ContactSection } from './contact-section/contact-section';
+import { AboutMe } from './about-me/about-me';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Carousel, ContactSection],
+  imports: [RouterOutlet, Navbar, Carousel, ContactSection, AboutMe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
