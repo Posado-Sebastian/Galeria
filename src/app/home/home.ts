bda0fec52d04859728384ee5c63e468e83f43290
Posado-Sebastian <sebastianposado@gmail.com>
2025-08-08 19:06:18 +0000

    titulos y boton

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -3,10 +3,11 @@ import { Banner } from '../banner/banner';
 import { Carousel } from '../carousel/carousel';
 import { AboutMe } from '../about-me/about-me';
 import { ContactSection } from '../contact-section/contact-section';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [Banner, Carousel, AboutMe, ContactSection],
+  imports: [Banner, Carousel, AboutMe, ContactSection, RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
