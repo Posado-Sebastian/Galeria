ef04cffce071d239e1eac40a163a41e1a98b6616
Posado-Sebastian <sebastianposado@gmail.com>
2025-08-10 02:03:47 +0000

    navbar falta mejorar como se ve en mobile

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.html',
-  styleUrls: ['./navbar.css']
+  styleUrls: ['./navbar.css'],
+  standalone: true,
+  imports: [CommonModule]
 })
 export class Navbar {
-  
+
 }
